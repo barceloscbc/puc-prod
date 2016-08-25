a599f16c6ac3f967e619819fd48e642b9decbc8b
Cleber Barcelos Costa <barcelos.cbc@gmail.com>
2016-08-25 17:46:57 -0300

    Configuração de porta de execução dos serviços.

--- exercicio4/build.sh
+++ exercicio4/build.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 mvn package
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /code/AmigoMicroService/target/Amigo-0.0.1-SNAPSHOT.jar;
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /code/ClienteMicroService/target/Cliente-0.0.1-SNAPSHOT.jar;
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /code/EmprestimoMicroService/target/Emprestimo-0.0.1-SNAPSHOT.jar;
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /code/PublicacaoMicroService/target/Publicacao-0.0.1-SNAPSHOT.jar;
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Dserver.port=8080 -jar /code/AmigoMicroService/target/Amigo-0.0.1-SNAPSHOT.jar;
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Dserver.port=8081 -jar /code/ClienteMicroService/target/Cliente-0.0.1-SNAPSHOT.jar;
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Dserver.port=8082 -jar /code/EmprestimoMicroService/target/Emprestimo-0.0.1-SNAPSHOT.jar;
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Dserver.port=8083 -jar /code/PublicacaoMicroService/target/Publicacao-0.0.1-SNAPSHOT.jar;
