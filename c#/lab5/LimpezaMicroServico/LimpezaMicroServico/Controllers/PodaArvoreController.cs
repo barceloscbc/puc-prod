9612a42a8c16b354d1033168eb715e4936453272
Cleber Barcelos <barcelos.cbc@gmail.com>
2016-08-25 22:00:43 -0300

    Desenvolvimento do laboratorio 5

---
+++ c#/lab5/LimpezaMicroServico/LimpezaMicroServico/Controllers/PodaArvoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace WebApiSwagger.Controllers
+{
+    [RoutePrefix("api/PodaArvore")]
+    public class PodaArvoreController : ApiController
+    {
+
+        [Route("{logradouro:alpha}", Name = "SolicitarPodaAvoreByLogradouro")]
+        public IHttpActionResult Post(string logradouro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ c#/lab5/LimpezaMicroServico/LimpezaMicroServico/Models/DiaColeta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Limpeza.Models
+{
+    public class DiaColeta
+    {
+      
+        [Required]
+        public string logradouro { get; set; }
+
+        [Required]
+        public string dia { get; set; }
+
+        public DiaColeta(string logradouro, string dia)
+        {
+            this.dia = dia;
+            this.logradouro = logradouro;
+        }
+    }
+}
